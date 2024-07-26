9916edb29edec9dd9b4f6d2f744e3a2f648d8c79
dev_one <sandeepm@epicmindsit.com>
2024-07-26 14:44:08 +0530

    Sponser Changes

--- InfoMed_API/InfoMed/Models/Sponsers.cs
+++ InfoMed_API/InfoMed/Models/Sponsers.cs
@@ -11,6 +11,7 @@ namespace InfoMed.Models
         public string SponsorType { get; set; }
         public string SponsorName { get; set; }
         public string? SponsorShowText { get; set; }
+        public string? SponsorUrl { get; set; }
         public string? SponsorLogo { get; set; }
         public int OrderNumber { get; set; }
         public bool Status { get; set; }
